16fa920d0f0d7595e1bf52751671419065d6b9ad
martin10ca <m.calderona@uniandes.edu.co>
2023-10-10 17:03:50 -0500

    distribucion consola

--- Entrega 3/App/src/consola/Consola.java
+++ Entrega 3/App/src/consola/Consola.java
@@ -1,8 +1,24 @@
 package consola;
 import inventario.Inventario;
 public class Consola {
+    public static void MenuInicial(){
+    //1) crear usuario tipo cliente
+    //2) iniciar sesion   
+    //admin (rf 1-14) (nico)
+    //admin local(15-17)
+    //cliente - personalatencion(19-20)
+    //personalatencion(en reserva->21,25-27)
+    //personalTecnico(20)
+    //cliente (24-27)
+    ;
+    }
+    public static void NuevoCliente(){
+    //josé
+    }
     public static void main(String[] args)
 	{
     Inventario.loadSistema();
+    MenuInicial();
+    
 	}
 }
