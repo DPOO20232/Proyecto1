9d0231388859b8f555a26fc2cd1d85d244ca22d3
Juan Arboleda <j.arboledam@uniandes.edu.co>
2023-11-13 17:57:06 -0500

    x

--- Entrega 3/App/src/vista/VentanaReserva.java
+++ Entrega 3/App/src/vista/VentanaReserva.java
@@ -13,6 +13,11 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentListener;
+
+import modelo.Categoria;
+import modelo.Inventario;
+import modelo.Sede;
+
 import javax.swing.event.DocumentEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,7 +39,7 @@ public class VentanaReserva extends JFrame {
         super("Reserva de Vehículo");
         tabbedPane = new JTabbedPane(); //Creación de la ventana
         
-        // Pestaña 1: Fecha y hora de recogida
+        // Pestaña 1: Fecha y hora de recogida y sede de recogida -----------------------------------------------------------------------------------------------------------------------------------------
         
         JPanel panel1 = new JPanel(new GridLayout(0,2));
         
@@ -54,7 +59,7 @@ public class VentanaReserva extends JFrame {
         panel1.add(textoBienvenida);
         panel1.add(fechaRecTitle);
        
-        // FECHA RECOGIDA ------------------------------------------------------------------------------------------------------------------------------------------
+        // Fecha de recogida 
         JPanel panelFechaRec = new JPanel();       
         panelFechaRec.setLayout(new FlowLayout());
         DefaultComboBoxModel<String> opcionesAnio = new DefaultComboBoxModel<>(); //Se crean y se agregan las opciones disponibles para el año de recogida
@@ -105,19 +110,42 @@ public class VentanaReserva extends JFrame {
         });
         panelFechaRec.add(anioBox); // Año agregado
         panel1.add(panelFechaRec);
-        //
-        //TODO: falta la hora de recogida
-        // ----------
         
+        // Hora de recogida
+        JPanel panelHoraRec = new JPanel();
+
+        String[] opcionesMinutosRec = {"00"};
+        DefaultComboBoxModel<String> opcionesHoraRec = new DefaultComboBoxModel<>();
+        
+        JComboBox<String> horaRecBox = new JComboBox<>(opcionesHoraRec);
+        JComboBox<String> minRecBox = new JComboBox<>(opcionesMinutosRec);
+                
+        horaRecBox.setSelectedIndex(0);
+        minRecBox.setSelectedIndex(0);
+
+        panelHoraRec.add(horaRecBox);
+        panelHoraRec.add(minRecBox);
+        panel1.add(panelHoraRec);
+
+        // Sede de recogida
+        JPanel panelSedeRec = new JPanel();
+        JComboBox<String> sedesRec = new JComboBox<>(); // El JComboBox se coloca en el centro del panel
+        for (Sede i : Inventario.getListaSedes()) {
+            sedesRec.addItem(Integer.toString(i.getID()) + ": " + i.getNombre());
+        }
+        sedesRec.setSelectedIndex(0);
+        panelSedeRec.add(sedesRec, BorderLayout.CENTER);
+        panel1.add(panelSedeRec);
+        
+        //Continuar
         panel1.add(botonContinuar);
         
-
-       // Pestaña 2: Fecha y hora de devolucion
+        // ----------
+        // Pestaña 2: Fecha y hora de devolucion -----------------------------------------------------------------------------------------------------------------------------------------
         
         JPanel panel2 = new JPanel(new GridLayout(0,2));
         
-        JLabel fechaDevTitle = new JLabel("Fecha de recogida del vehículo");
-        
+        panel2.add(new JLabel("Fecha de recogida del vehículo"));
         botonContinuar = new JButton("Continuar");
         botonContinuar.addActionListener(new ActionListener() {
             @Override
@@ -126,9 +154,8 @@ public class VentanaReserva extends JFrame {
             }
         });
         
-        panel2.add(fechaDevTitle);
-       
-        // FECHA RECOGIDA ------------------------------------------------------------------------------------------------------------------------------------------
+        // Fecha devolución
+
         JPanel panelFechaDev = new JPanel();       
         panelFechaRec.setLayout(new FlowLayout());
         DefaultComboBoxModel<String> opcionesAnio2 = new DefaultComboBoxModel<>(); //Se crean y se agregan las opciones disponibles para el año de recogida
@@ -159,11 +186,9 @@ public class VentanaReserva extends JFrame {
                 saveDatebutton.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e){
-                        
                         VentanaMain.refresh(panelFechaDev);
                         panelFechaDev.add(anioBox2);
                         panelFechaDev.add(updateDatebutton);
-                       
                     }
                 });
                 updateDatebutton.addActionListener(new ActionListener() {
@@ -179,11 +204,55 @@ public class VentanaReserva extends JFrame {
         });
         panelFechaDev.add(anioBox2); // Año agregado
         panel2.add(panelFechaDev);
-        //
-        //TODO: falta la hora de recogida
+
+        //Hora de devolución
+        JPanel panelHoraDev = new JPanel();
+
+        String[] opcionesMinutosDev = {"00"};
+        DefaultComboBoxModel<String> opcionesHoraDev = new DefaultComboBoxModel<>();
+        
+        JComboBox<String> horaDevBox = new JComboBox<>(opcionesHoraDev);
+        JComboBox<String> minDevBox = new JComboBox<>(opcionesMinutosDev);
+                
+        horaDevBox.setSelectedIndex(0);
+        minDevBox.setSelectedIndex(0);
+        panelHoraDev.add(horaDevBox);
+        panelHoraDev.add(minDevBox);
+        panel2.add(panelHoraDev);
+
+        // Sede de devolución
+        JPanel panelSedeDev = new JPanel();
+        JComboBox<String> sedesDev = new JComboBox<>(); // El JComboBox se coloca en el centro del panel
+        for (Sede i : Inventario.getListaSedes()) {
+            sedesDev.addItem(Integer.toString(i.getID()) + ": " + i.getNombre());
+        }
+        sedesDev.setSelectedIndex(0);
+        panelSedeDev.add(sedesDev, BorderLayout.CENTER);
+        panel2.add(panelSedeDev);
+        
+        //Continuar
+        panel2.add(botonContinuar);
+        
         // ----------
-        tabbedPane.add("Fecha recogida", panel1);
-        tabbedPane.add("Fecha devolucion", panel2);
+        // Pestaña 3: Seleccionar categoría -----------------------------------------------------------------------------------------------------------------------------------------
+        JPanel panel3 = new JPanel();
+        panel3.setPreferredSize(new Dimension(200, 400)); // Ajustar las dimensiones 
+        panel3.setLayout(new BorderLayout(20, 0));
+        panel3.add(new JLabel("Seleccione la categoría"), BorderLayout.NORTH);// El JLabel se coloca en la parte superior del panel
+        
+        JPanel panelCategorias = new JPanel();
+        JComboBox<String> categBox = new JComboBox<>(); // El JComboBox se coloca en el centro del panel
+        for (Categoria i : Inventario.getListaCategorias()) {
+            categBox.addItem(Integer.toString(i.getID()) + ": " + i);
+        }
+        categBox.setSelectedIndex(0);
+        panelCategorias.add(categBox, BorderLayout.CENTER);
+        
+        panel3.add(panelCategorias);
+        tabbedPane.add("Información de recogida", panel1);
+        tabbedPane.add("Información de devolucion", panel2);
+        tabbedPane.add("Selección de categoría", panel3);
+
         add(tabbedPane);
         setLocationRelativeTo(null);
         setSize(840, 600);
