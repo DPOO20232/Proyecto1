183151e8caa29aade7147bd46c8cf4715e1ad512
jjhc726 <jj.hurtadoc1@uniandes.edu.co>
2023-12-07 18:32:55 -0500

    cambios

--- Entrega 3/App/src/modelo/Facturas.java
+++ Entrega 3/App/src/modelo/Facturas.java
@@ -1,46 +1,70 @@
 package modelo;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
+import java.io.File;
 import java.io.FileOutputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 
 public class Facturas {
 
     public static void facturas(alquiler alquiler) {
         try {
             // Crear un documento PDF
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream("./facturas/" + alquiler.getID() + ".pdf"));
-            document.open();
+            File file=new File("./facturas/" + alquiler.getID() + ".pdf");
+            PdfWriter pdfWriter=new PdfWriter(file);
+            PdfDocument pdfDocument=new PdfDocument(pdfWriter);
+            Document document = new Document(pdfDocument);
+
 
             // Contenido de la factura
-            document.add(new Paragraph("\t\t" + Inventario.getNombreCompania()));
-            document.add(new Paragraph("\t\t Id Factura: " + alquiler.getID()));
+            document.add(new Paragraph("        " + Inventario.getNombreCompania()));
+            document.add(new Paragraph("         Id Factura: " + alquiler.getID()));
             document.add(new Paragraph("------------------------------------------------------------------------ "));
             document.add(new Paragraph("\n\n\n"));
             LocalDate fechaActual = LocalDate.now();
             document.add(new Paragraph("Fecha: " + fechaActual));
-            document.add(new Paragraph("Direccion: " + alquiler.getReserva().getSedeEntregar()));
+            document.add(new Paragraph("Direccion: " + alquiler.getReserva().getSedeEntregar().getUbicacion()));
             document.add(new Paragraph("\n------------------------------------------------------------------------"));
+            document.add(new Paragraph("\n\n\n"));
             document.add(new Paragraph("Datos Cliente: "));
             document.add(new Paragraph("Nombre: " + alquiler.getReserva().getCliente().getNombre()));
             document.add(new Paragraph("Telefono: " + alquiler.getReserva().getCliente().getTelefono()));
             document.add(new Paragraph("Mail: " + alquiler.getReserva().getCliente().getMail()));
             document.add(new Paragraph("\n------------------------------------------------------------------------ "));
-            document.add(new Paragraph("Descripcion: "));
-            document.add(new Paragraph("\t\t\t\t\t\t\t\t\tMonto: " ));
-            document.add(new Paragraph("Alquiler vehiculo tipo: " + alquiler.getReserva().getCategoria()));
-            document.add(new Paragraph("\t\t\t"+Double.toString(alquiler.getPagoFinal())));
-            document.add(new Paragraph("por " + Reserva.calcularDuracionRenta(alquiler.getReserva().getFechaRecoger(),
+            document.add(new Paragraph("\n\n\n"));
+            document.add(new Paragraph("Descripcion:                        Monto:"));
+            document.add(new Paragraph("Alquiler vehiculo tipo: "+"         "+Double.toString(alquiler.getPagoFinal())));
+            document.add(new Paragraph(alquiler.getReserva().getCategoria().getnombreCategoria()));
+            document.add(new Paragraph("por " + Integer.toString(Reserva.calcularDuracionRenta(alquiler.getReserva().getFechaRecoger(),
                     alquiler.getReserva().getHoraRecoger(), alquiler.getReserva().getFechaEntregar(),
-                    alquiler.getReserva().getHoraEntregar()) + " días."));
+                    alquiler.getReserva().getHoraEntregar())) + " días."));
             document.add(new Paragraph("------------------------------------------------------------------------ "));
+             // Agregar imagen en la parte inferior de la página
+            String imagePath = "C:\\Users\\USUARIO\\Documents\\Proyecto1\\imagenes\\Firma.png"; // Reemplaza con la ruta de tu imagen
+            Image img = new Image(ImageDataFactory.create(imagePath));
+            img.setWidth(100); // Ajusta el ancho de la imagen según tus necesidades
+            img.setTextAlignment(TextAlignment.CENTER);
 
+            // Obtener la última página del documento
+            PdfPage lastPage = pdfDocument.getLastPage();
+
+            // Agregar la imagen en la parte inferior
+            float x = (lastPage.getPageSize().getLeft() + lastPage.getPageSize().getRight()) / 2;
+            float y = lastPage.getPageSize().getBottom() + 30; // Puedes ajustar la posición vertical según tus necesidades
+            img.setFixedPosition(x, y);
+
+            // Agregar la imagen al documento
+            document.add(img);
             // Cerrar el documento
             document.close();
 
@@ -64,6 +88,6 @@ public class Facturas {
         Facturas factura = new Facturas(); // Asegúrate de tener una clase Alquiler con un constructor apropiado
         Reserva reserva= new Reserva(20231215, 20231219, 1300, 1400, false, sedeRecoger, sedeEntregar, categoria, cliente);
         alquiler alquiler=new alquiler(reserva);
-        facturas(alquiler);
+        factura.facturas(alquiler);
     }
 }
